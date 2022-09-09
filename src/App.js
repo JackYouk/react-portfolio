4bc15fd8871716af20e5f58b19c059e68ec8267b
JackYouk <jackyoukstetter1@gmail.com>
2022-09-08 20:01:04 -0700

    switched out bootstrap for material ui and deleted navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Example from "./Navbar";
 
 function App() {
   return (
     <>
     {/* Navbar */}
-    <Example />
     {/* Hero Image */}
     {/*  */}
     </>
