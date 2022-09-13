b63ee36d071f78fd39c2974adcdcc62edc8cff5c
JackYouk <jackyoukstetter1@gmail.com>
2022-09-12 23:46:38 -0700

    fixed mobile responsiveness for resume modal

--- src/Header.js
+++ src/Header.js
@@ -19,7 +19,6 @@ const Header = () => {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: 400,
         border: '2px solid #000',
       };
 
@@ -59,7 +58,7 @@ const Header = () => {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={modalStyle}>
+                <Box sx={modalStyle} id='modalBox'>
                     <img src='/resume.png' alt='resume' />
                 </Box>
             </Modal>
